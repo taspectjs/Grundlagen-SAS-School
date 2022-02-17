a2f0735514702fc6527478d231e4ba622dcb3eb3
Luca Schröder <luca.s44@gso.schule.koeln>
2022-02-17 13:54:41 +0100

    Fertig

--- Aufgabe 3/Program.cs
+++ Aufgabe 3/Program.cs
@@ -7,15 +7,26 @@ namespace Aufgabe_3
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Geben sie den Schrittgröße ein!");
+            Console.Write("Geben sie den Schrittgröße ein: ");
             double schrittgröße = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Geben sie den Abstand ein!");
+            Console.Write("\nGeben sie den Abstand ein: ");
             double abstand = Convert.ToDouble(Console.ReadLine());
 
             double p = 0.017, A = 0.068;
-            double R = p*(/A)
+            double l = schrittgröße * abstand;
+            double R = p * (l/ A);
+
+            string abst = "Wiederabstand", meter = "Meter";
+                
+            Console.Write("\n{0,6}",meter); Console.Write("{0,17}\n",abst);
+            Console.Write("{0,4:G}m", l); Console.Write("{0,15:G}hm",R);
+
+            Console.ReadKey();
             
+
+
+
 
 
 
