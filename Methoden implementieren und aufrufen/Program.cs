6d7b0b14e46754369c4725d5c5785589d97d2a91
Luca Schröder <luca.s44@gso.schule.koeln>
2022-03-31 11:36:27 +0200

    Methoden implementirt

--- Methoden implementieren und aufrufen/Program.cs
+++ Methoden implementieren und aufrufen/Program.cs
@@ -6,7 +6,6 @@ namespace Methoden_implementieren_und_aufrufen
     {
         public static void Main()
         {
-            eigabe(text);
 
             int anzahl = 0;
             anzahl = ausgabeTextMalX("das ist sehr schwer", 10);
@@ -22,15 +21,6 @@ namespace Methoden_implementieren_und_aufrufen
 
             }
             return text.Length;
-
-        }
-
-        public static string eingabe(string text)
-        {
-            Console.WriteLine("Geben sie den Text ein der ausgegeben wird!");
-            text = Console.ReadLine();
-
-            return text;
 
         }
     }
