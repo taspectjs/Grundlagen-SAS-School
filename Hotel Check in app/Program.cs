b4055b12a021731c593e2e0c6b1d899193970c35
Luca Schröder <luca.s44@gso.schule.koeln>
2022-04-06 14:36:02 +0200

    Hotel update

---
+++ Hotel Check in app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hotel_Check_in_app
+{
+    class Program
+    {
+        static void Hotelcheck()
+        {
+            Console.WriteLine("Bitte geben Sie Nachnamen oder Zimmernummer ein!");
+            string eingabe_str = Console.ReadLine();
+            short eingabe_short;
+
+            bool flag = short.TryParse(eingabe_str, out eingabe_short);
+
+        }
+
+        static void DATAGet()
+        {
+            short[] zimmernummer = { 45, 23, 3, 32 };
+            string[] Vorname = { "John", "Paul", "George", "Ringo" };
+            string[] Nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
+            short[] Übernachtung = { 1, 4, 12, 3 };
+            double[] rechnungsbetrag = { 82, 48, 329.48, 989.52, 247.38 };
+
+
+        }
+
+    }
+}
